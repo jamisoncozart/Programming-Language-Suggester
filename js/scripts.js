d70aeeb5084c9762842747a4ad247b9f9b1afb13
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-17 08:49:39 -0800

    Add input value array looping to tally language scores

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,19 @@ $(document).ready(function() {
     var q1Value = $("input:radio[name=make]:checked").val();
     var q2Value = $("input:radio[name=syntax]:checked").val();
 
-    console.log(q1Value + " " + q2Value);
+    var valueArray = [q1Value, q2Value];
+
+    for(let i = 0; i < valueArray.length; i++) {
+      if(valueArray[i] === "js") {
+        jsScore++;
+      } else if(valueArray[i] === "cs") {
+        csScore++;
+      } else if(valueArray[i] === "py") {
+        pyScore++;
+      } else {
+        console.log("Something went wrong");
+      }
+    }
+    console.log("Javascript: " + jsScore + " | C#: " + csScore + " | Python: " + pyScore);
   })
 })
