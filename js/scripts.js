3642087ef47609773f1e988798920a4a4b395448
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-17 09:46:02 -0800

    Hide and show HTML results based on language scores

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,10 @@ $(document).ready(function() {
     //Store all form input values
     var q1Value = $("input:radio[name=make]:checked").val();
     var q2Value = $("input:radio[name=syntax]:checked").val();
+    var q3Value = $("input:radio[name=industry]:checked").val();
+    var q4Value = $("input:radio[name=popular]:checked").val();
     //Loop through all question values and add 1 to language score.
-    var valueArray = [q1Value, q2Value];
+    var valueArray = [q1Value, q2Value, q3Value, q4Value];
     for(let i = 0; i < valueArray.length; i++) {
       if(valueArray[i] === "js") {
         jsScore++;
@@ -22,5 +24,26 @@ $(document).ready(function() {
     }
     
     //Display best-fit language based on user scores
+    if(jsScore >= pyScore && jsScore >= csScore && csScore >= pyScore) {
+      $("#mainTitle").hide();
+      $("#mainSubheading").hide();
+      $("#survey").hide();
+      $("#languageName").text("Javascript");
+      $("#scoreList").append("<li>Javascript: " + jsScore + "</li>");
+      $("#scoreList").append("<li>C#: " + csScore + "</li>");
+      $("#scoreList").append("<li>Python: " + pyScore + "</li>");
+      $("#cs").hide();
+      $("#py").hide();
+    } else if(jsScore >= pyScore && jsScore >= csScore && pyScore >= csScore) {
+      $("#mainTitle").hide();
+      $("#mainSubheading").hide();
+      $("#survey").hide();
+      $("#languageName").text("Javascript");
+      $("#scoreList").append("<li>Javascript: " + jsScore + "</li>");
+      $("#scoreList").append("<li>Python: " + pyScore + "</li>");
+      $("#scoreList").append("<li>C#: " + csScore + "</li>");
+      $("#cs").hide();
+      $("#py").hide();
+    }
   })
 })
